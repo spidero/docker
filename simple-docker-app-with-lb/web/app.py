2615299c8e782930441db886ea160c7a4f6b5e20
Piotr Skwarna <piotr@skwarna.eu>
2023-08-16 09:28:30 +0200

    simple python app with roudrobin loadbalancer in nginx

---
+++ simple-docker-app-with-lb/web/app.py
@@ -0,0 +1,28 @@
+import time
+import socket
+import redis
+
+from flask import Flask, request
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route("/hostname/")
+def return_hostname():
+    return "This is an example wsgi app served from {} to {}".format(socket.gethostname(), request.remote_addr)
